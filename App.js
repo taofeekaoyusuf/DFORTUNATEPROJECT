c0185541aca10c005226098cf41386d309bfc514
Taofeek Abiodun Olasunkanmi Yusuf <labatevry@gmail.com>
2024-11-25 00:40:25 +0100

    new updates added to repo

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import AdminDashboard from './pages/AdminDashboard';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const App = () => {
   return (
