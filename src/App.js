c76b1a292a5efa53ed98550869234ecd32a3c862
Taofeek Abiodun Olasunkanmi Yusuf <labatevry@gmail.com>
2024-11-25 01:20:00 +0100

    Code files updated to repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
-import AdminDashboard from './pages/AdminDashboard';
+import Login from '../pages/Login';
+import AdminDashboard from '../pages/AdminDashboard';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const App = () => {
